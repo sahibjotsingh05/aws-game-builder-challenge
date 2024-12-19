4d7084b331454e087ccf77c715cb28dc8a3ce59e
Sahibjot Singh <75125850+sahibjotsingh05@users.noreply.github.com>
2024-12-19 00:52:38 -0700

    Animation for Main Menu Screens

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,83 @@
-// Handle game layers here
+$(document).ready(function () {
+  $(".blur-layer").css({
+    "backdrop-filter": "blur(4px)",
+    "-webkit-backdrop-filter": "blur(4px)",
+  });
+  $("#upperparchment-layer").hide();
+  $("#lowerparchment-layer").hide();
+  $("#parchment-layer").hide();
+  $("#loader-layer").css("display", "none");
+  $("#layer2").fadeIn(2000);
 
-// Create a blur layer
+  function dropParchment(type) {
+    // upper parchment layer should go to top -50vh. initially at 50vh. after that -50 to -87
+    //bottom one should go to top 50vh. initially at top 150vh. after that 50vh to 87
+    var htmlString =
+      "<button class='closeParchmentBtn'>Close</button><h2>Type: " +
+      type +
+      "</h2>";
+    $("#parchment").empty();
+    $("#parchment").append(htmlString);
+    $("#upperparchment-layer").show();
+    $("#lowerparchment-layer").show();
+    $("#upperparchment-layer").css({
+      animation: "moveUpperLayer 1s forwards",
+    });
+    $("#lowerparchment-layer").css({
+      animation: "moveLowerLayer 1s forwards",
+    });
+
+    $("#upperparchment-layer").one("animationend", function () {
+      $("#upperparchment-layer").css({
+        animation: "moveUpperLayerSecond 1s forwards",
+      });
+      $("#layer2").fadeOut(500);
+      $("#parchment-layer").show();
+      $("#parchment").css({
+        animation: "moveParchment 1s forwards",
+      });
+    });
+    $("#lowerparchment-layer").one("animationend", function () {
+      $("#lowerparchment-layer").css({
+        animation: "moveLowerLayerSecond 1s forwards",
+      });
+    });
+  }
+  function hideParchment() {
+    $("#upperparchment-layer").css({
+      animation: "reverseUpperLayerSecond 1s forwards",
+    });
+    $("#parchment").css({
+      animation: "reverseParchment 1s forwards",
+    });
+    $("#lowerparchment-layer").css({
+      animation: "reverseLowerLayerSecond 1s forwards",
+    });
+    $("#upperparchment-layer").one("animationend", function () {
+      $("#layer2").fadeIn(500);
+      $("#upperparchment-layer").css({
+        animation: "reverseUpperLayer 1s forwards",
+      });
+      $("#lowerparchment-layer").css({
+        animation: "reverseLowerLayer 1s forwards",
+      });
+      $("#upperparchment-layer").one("animationend", function () {
+        $("#upperparchment-layer").hide();
+        $("#lowerparchment-layer").hide();
+        $("#parchment-layer").hide();
+      });
+    });
+  }
+  $("#play-with-friends").click(function () {
+    dropParchment(0);
+  });
+  $("#play-against-ai").click(function () {
+    dropParchment(1);
+  });
+  $("#how-to-play").click(function () {
+    dropParchment(2);
+  });
+  $(document).on("click", ".closeParchmentBtn", function () {
+    hideParchment();
+  });
+});
